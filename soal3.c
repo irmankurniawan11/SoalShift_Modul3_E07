efd6a715f39b4699bf7e6bb7eec8ee333582813e
Irman Kurniawan <irmankurniawan99@gmail.com>
2017-10-24 21:05:30 +0700

    Melengkapi fungsi menu

--- soal3.c
+++ soal3.c
@@ -4,16 +4,23 @@
 pthread_t menu_t, lohan_t, kepiting_t;
 int lohan_st = 100;
 int kepiting_st = 100;
+int KO = 0;
 
 void *menu() {
 	printf("MENU\n1 = BERIMAKAN LOHAN\n2 = BERIMAKAN KEPITING\n");
 	int x;
 	scanf("%d",&x);
 	if (x==1) {
-		break;
+		lohan_st += 10;
+		if (lohan_st<=0 || lohan_st>100) {
+			KO = 1;break;		
+		}
 	}
 	else if (x==2) {
-		break;
+		kepiting_st += 10;
+		if (kepiting_st<=0 || kepiting_st>100) {
+			KO = 1;break;
+		}
 	}
 	else continue;
 }
