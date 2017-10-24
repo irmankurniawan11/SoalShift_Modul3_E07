492b4e915fb1bd3efe210c86f01d863f6f8c3a6c
ivanderwilliam <ivanderwilliam@gmail.com>
2017-10-25 00:39:11 +0700

    sikit2 fungsi dasarnya

---
+++ soal1_penjual.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/wait.h>
+#include<string.h>
+
+void char senjata[6][6]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
+int stock[6]={"0","1","2","3","4","5"};
+
+int main(){
+
