9b8afd74b7ed12fddb1003f5b4e1ed90a505e847
Irman Kurniawan <irmankurniawan99@gmail.com>
2017-10-24 21:33:20 +0700

    Rapih-rapih, nambah print GAME BERAKHIR pada saat game telah selesai

--- soal3.c
+++ soal3.c
@@ -9,29 +9,31 @@ int kepiting_st = 100;
 int KO = 0;
 
 void *menu() {
-while(1) {
-	printf("MENU\n1 = BERIMAKAN LOHAN\n2 = BERIMAKAN KEPITING\n");
-	int x;
-	scanf("%d",&x);
-	if (x==1) {
-		lohan_st += 10;
-		printf("Lohan : %d | Kepiting : %d\n",lohan_st,kepiting_st);
-		if (lohan_st<=0 || lohan_st>100) {
-			KO = 1;break;		
+	while(1) {
+		printf("MENU\n1 = BERIMAKAN LOHAN\n2 = BERIMAKAN KEPITING\n");
+		int x;
+		scanf("%d",&x);
+		if (x==1) {
+			lohan_st += 10;
+			printf("Lohan : %d | Kepiting : %d\n",lohan_st,kepiting_st);
+			if (lohan_st<=0 || lohan_st>100) {
+				KO = 1;
+				break;		
+			}
+		}
+		else if (x==2) {
+			kepiting_st += 10;
+			printf("Lohan : %d | Kepiting : %d\n",lohan_st,kepiting_st);
+			if (kepiting_st<=0 || kepiting_st>100) {
+				KO = 1;
+				break;
+			}
+		}
+		else {
+			printf("Lohan : %d | Kepiting : %d\n",lohan_st,kepiting_st);
+			continue;
 		}
 	}
-	else if (x==2) {
-		kepiting_st += 10;
-		printf("Lohan : %d | Kepiting : %d\n",lohan_st,kepiting_st);
-		if (kepiting_st<=0 || kepiting_st>100) {
-			KO = 1;break;
-		}
-	}
-	else {
-		printf("Lohan : %d | Kepiting : %d\n",lohan_st,kepiting_st);
-		continue;
-	}
-}
 }
 
 void *lohan() {
@@ -39,7 +41,8 @@ void *lohan() {
 		sleep(10);
 		lohan_st -= 15;
 		if (lohan_st<=0 || lohan_st>100) {
-			KO = 1;break;		
+			KO = 1;
+			break;		
 		}
 	}
 }
@@ -49,7 +52,8 @@ void *kepiting() {
 		sleep(20);
 		kepiting_st -= 10;
 		if (kepiting_st<=0 || kepiting_st>100) {
-			KO = 1;break;
+			KO = 1;
+			break;
 		}
 	}
 }
@@ -60,7 +64,10 @@ int main() {
 	pthread_create(&(kepiting_t),NULL,&kepiting,NULL);
 
 	while(1) {
-		if(KO==1) break;
+		if(KO==1) {
+			printf("GAME BERAKHIR\n");
+			break;
+		}
 	}
 	return 0;
 }
