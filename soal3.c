e80ceea9de51fdc226070f2bf1fd2dd1864015d3
Irman Kurniawan <irmankurniawan99@gmail.com>
2017-10-24 21:00:44 +0700

    Membuat fungsi menu (pilihan menu) dan status lohan kepiting

--- soal3.c
+++ soal3.c
@@ -2,22 +2,37 @@
 #include <pthread.h>
 
 pthread_t menu_t, lohan_t, kepiting_t;
+int lohan_st = 100;
+int kepiting_st = 100;
 
 void *menu() {
-
+	printf("MENU\n1 = BERIMAKAN LOHAN\n2 = BERIMAKAN KEPITING\n");
+	int x;
+	scanf("%d",&x);
+	if (x==1) {
+		break;
+	}
+	else if (x==2) {
+		break;
+	}
+	else continue;
 }
 
 void *lohan() {
-
+	while(1) {
+		
+	}
 }
 
 void *kepiting() {
-
+	while(1) {
+		
+	}
 }
 
 int main() {
 	pthread_create(&(menu_t),NULL,&menu,NULL);
 	pthread_create(&(menu_t),NULL,&menu,NULL);
 	pthread_create(&(menu_t),NULL,&menu,NULL);
-return 0;
+	return 0;
 }
