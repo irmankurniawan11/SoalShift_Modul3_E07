540f208760dd09cf2bd0b932f19cd07159c5a435
ivanderwilliam <ivanderwilliam@gmail.com>
2017-10-25 14:27:29 +0700

    penambahan fungsi main

--- soal5.c
+++ soal5.c
@@ -6,12 +6,14 @@
 pthread_t tid1;
 pthread_t tid2;
 
-int main(){
+int main(int temp,char *argv[]){
+pthread_t tid[isi];
 for(int x=1;x<;x++){
-pthread_create
+pthread_create(&(tid[x],NULL,&fungsinya,(void *) argv[x]);
 }
 
 for(int x=1;x<;x++){
-pthread_join
+pthread_join(tid[x],NULL);
 }
+
 }
