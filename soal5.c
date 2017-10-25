b0f69276b7c4b55a87829e367ecf2ed76fbf31c7
ivanderwilliam <ivanderwilliam@gmail.com>
2017-10-25 14:20:01 +0700

    main

---
+++ soal5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<string.h>
+#include<unistd.h>
+
+pthread_t tid1;
+pthread_t tid2;
+
+int main(){
+for(int x=1;x<;x++){
+pthread_create
+}
+
+for(int x=1;x<;x++){
+pthread_join
+}
+}
