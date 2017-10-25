6afd508ce6f8c5e4bb992133b1d5c065ce91684a
ivanderwilliam <ivanderwilliam@gmail.com>
2017-10-25 12:45:11 +0700

    sesuaikan sama yang pembeli

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -4,24 +4,30 @@
 #include<sys/wait.h>
 #include<string.h>
 
-void char senjata[6][6]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
-int stock[6]={"0","1","2","3","4","5"};
+void char senjata[10][10]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"}
 
 int main(){
 int choice;
-scanf("%d",&choice)
+int shmid = shmget(keyp,sizeof(int)*6, IPC_CREAT | 0666);
+int *stock=shmat(shmid,NULL,0);
+int jumlah;
+scanf("%d",&choice);
 if(choice==1){
 	for(int x=0;x<6;x++){
 		if(stock[x]!=0){
-		printf("%s %d",senjata[x],stock[x]
+		printf("%s %d",senjata[x],stock[x]);
 		}
 	}
 char nama[30];
 if(choice==2){
-scanf("%s %d",&nama,&jumlah);
+scanf("%s %d",nama,&jumlah);
 	for(int x=0;x<6;x++){
 	if(!strcmp(senjata[x],nama){
 		stock[x]=stock[x]+jumlah;
 		break;
 	}}
+}
+shmdt(stock);
+shmctl(shmid, IPC_RMID, NULL);
+
 }
