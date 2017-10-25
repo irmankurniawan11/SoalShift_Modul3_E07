1846e68bab122256f2e216d8317308f4d6716995
Irman Kurniawan <irmankurniawan99@gmail.com>
2017-10-25 12:43:38 +0700

    Membuat scan nama pada tiap fungsi player

--- soal2.c
+++ soal2.c
@@ -5,16 +5,30 @@
 pthread_t p1,p2;
 int status;
 int mine1[16], mine2[16];
-
+int nama1[50], nama2[50];
 
 void *fungsi1() {
 	int i;
+	int step=0;
 	for(i=0; i<16;i++) mine1[i]=0;
+	while(1) {
+		if(step==0) {
+			scanf("%s",nama1);
+			step=1;
+		}
+	}
 }
 
 void *fungsi2() {
 	int i;
+	int step=0;
 	for(i=0; i<16;i++) mine1[i]=0;
+	while(1) {
+		if(step==0) {
+			scanf("%s",nama2);
+			step=1;
+		}
+	}
 }
 
 int main() {
