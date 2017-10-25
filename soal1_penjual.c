32665e66c2ba36c40076f08f2a5a56684cb098ad
ivanderwilliam <ivanderwilliam@gmail.com>
2017-10-25 13:01:32 +0700

    perbaikan error

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -3,26 +3,29 @@
 #include<sys/ipc.h>
 #include<sys/wait.h>
 #include<string.h>
-
-void char senjata[10][10]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"}
+#include<sys/shm.h>
+#include<unistd.h>
+char senjata[10][10]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
 
 int main(){
+key_t keyp=1234;
 int choice;
 int shmid = shmget(keyp,sizeof(int)*6, IPC_CREAT | 0666);
 int *stock=shmat(shmid,NULL,0);
 int jumlah;
+while(1){
 scanf("%d",&choice);
 if(choice==1){
 	for(int x=0;x<6;x++){
 		if(stock[x]!=0){
 		printf("%s %d",senjata[x],stock[x]);
 		}
-	}
+	}}
 char nama[30];
 if(choice==2){
 scanf("%s %d",nama,&jumlah);
 	for(int x=0;x<6;x++){
-	if(!strcmp(senjata[x],nama){
+	if(!strcmp(senjata[x],nama)){
 		stock[x]=stock[x]+jumlah;
 		break;
 	}}
@@ -30,4 +33,5 @@ scanf("%s %d",nama,&jumlah);
 shmdt(stock);
 shmctl(shmid, IPC_RMID, NULL);
 
+}
 }
