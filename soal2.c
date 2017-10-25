d80d986d2075fda8ba2445475b6eaed118189599
Irman Kurniawan <irmankurniawan99@gmail.com>
2017-10-25 12:56:48 +0700

    Menambah variable turn supaya bisa giliran setelah p1 selesai

--- soal2.c
+++ soal2.c
@@ -6,15 +6,18 @@ pthread_t p1,p2;
 int status;
 int mine1[16], mine2[16];
 int nama1[50], nama2[50];
+int turn=1;
 
 void *fungsi1() {
 	int i;
 	int step=0;
 	for(i=0; i<16;i++) mine1[i]=0;
 	while(1) {
+		while(turn!=1) {}
 		if(step==0) {
 			scanf("%s",nama1);
 			step=1;
+			turn=2;
 		}
 	}
 }
@@ -24,9 +27,11 @@ void *fungsi2() {
 	int step=0;
 	for(i=0; i<16;i++) mine1[i]=0;
 	while(1) {
+		while(turn!=2) {}
 		if(step==0) {
 			scanf("%s",nama2);
 			step=1;
+			turn=1;
 		}
 	}
 }
