7923d4ab12934b0b24eecd5620d3dedf45ce2cad
ivanderwilliam <ivanderwilliam@gmail.com>
2017-11-03 09:51:59 +0700

    ku ketik ulang tiba2 bisa

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/ipc.h>
-#include<sys/wait.h>
-#include<string.h>
-#include<sys/shm.h>
-#include<unistd.h>
-char senjata[10][10]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/wait.h>
+
+char senjata[6][10]={"MPM4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
 
 int main(){
-key_t keyp=1234;
-int choice;
-int shmid = shmget(keyp,sizeof(int)*6, IPC_CREAT | 0666);
-int *stock=shmat(shmid,NULL,0);
-int jumlah;
-while(1){
-scanf("%d",&choice);
-if(choice==1){
-	for(int x=0;x<6;x++){
-		if(stock[x]!=0){
-		printf("%s %d",senjata[x],stock[x]);
-		}
-	}}
-char nama[30];
-if(choice==2){
-scanf("%s %d",nama,&jumlah);
-	for(int x=0;x<6;x++){
-	if(!strcmp(senjata[x],nama)){
-		stock[x]=stock[x]+jumlah;
-		break;
-	}}
-}
-shmdt(stock);
-shmctl(shmid, IPC_RMID, NULL);
 
-}
+	key_t keyp= 1234;
+	int shmid = shmget(keyp,sizeof(int)*6, IPC_CREAT | 0666);
+	int *stock=shmat(shmid,NULL,0);	
+	int choice;
+	int jumlah;
+	char nama[10];
+	while(1){
+		scanf("%d",&choice);
+		if(choice==1){
+			for(int i=0;i<6;i++){
+				if(stock[i]>0) printf("%s %d\n",senjata[i],stock[i]);
+			}
+		}
+		if(choice==2){
+			scanf("%s %d",nama,&jumlah);
+			for(int i=0;i<6;i++){
+				if(!strcmp(senjata[i],nama)){
+					stock[i]+=jumlah;
+				}
+			}
+		}
+	}
+	shmdt(stock);
+	shmctl(shmid,IPC_RMID,NULL);
+
 }
